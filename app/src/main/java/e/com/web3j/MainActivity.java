3c0dfb2c04e60a97d7bbe297e725000de4685c66
wjgkingsley <2207409996@qq.com>
2018-07-04 15:30:25 +0800

    1.0

--- app/src/main/java/e/com/web3j/MainActivity.java
+++ app/src/main/java/e/com/web3j/MainActivity.java
@@ -1,13 +1,40 @@
 package e.com.web3j;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import e.com.web3j.base.Config;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button createAccount = findViewById(R.id.create_account);
+        editText = findViewById(R.id.create_account_password);
+        createAccount.setOnClickListener(this);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.create_account:
+                String password = editText.getText().toString().trim();
+                if (password == null || "".equals(password)){
+                    editText.setError("请输入密码");
+//                    Toast.makeText(this,"密码不能为空",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(this,password,Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
     }
 }
---
+++ app/src/main/java/e/com/web3j/base/Config.java
@@ -0,0 +1,12 @@
+package e.com.web3j.base;
+
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.JsonRpc2_0Web3j;
+import org.web3j.protocol.http.HttpService;
+
+public class Config {
+
+    final private static String url = "http://192.168.2.37:8545";
+    final public static Web3j web3j = new JsonRpc2_0Web3j(new HttpService(url));
+
+}
